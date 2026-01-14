263d8edf1ae7b7c80d78b34969c98a8d415a1099
Loner459 <l2859794@gmail.com>
2026-01-14 19:19:27 +0800

    Create astro-icon.d.ts

---
+++ src/types/astro-icon.d.ts
@@ -0,0 +1,6 @@
+declare module "astro-icon/components" {
+  // Minimal typings for astro-icon subpath used by this project.
+  // `Icon` is an Astro component exported by the package. Using `any` to avoid stricter ASTRO type dependencies.
+  export const Icon: any;
+  export default Icon;
+}
