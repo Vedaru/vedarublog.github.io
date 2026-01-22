c8407c0a8c1c22f968b1f3f242f6f348a61dab31
Loner459 <l2859794@gmail.com>
2026-01-22 13:42:10 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -616,11 +616,11 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 
 export const sakuraConfig: SakuraConfig = {
 	enable: false, // 默认关闭樱花特效
-	sakuraNum: 21, // 樱花数量
+	sakuraNum: 400, // 樱花数量
 	limitTimes: -1, // 樱花越界限制次数，-1为无限循环
 	size: {
-		min: 0.5, // 樱花最小尺寸倍数
-		max: 1.1, // 樱花最大尺寸倍数
+		min: 0.2, // 樱花最小尺寸倍数
+		max: 0.3, // 樱花最大尺寸倍数
 	},
 	opacity: {
 		min: 0.3, // 樱花最小不透明度
@@ -628,12 +628,12 @@ export const sakuraConfig: SakuraConfig = {
 	},
 	speed: {
 		horizontal: {
-			min: -1.7, // 水平移动速度最小值
-			max: -1.2, // 水平移动速度最大值
+			min: -0.5, // 水平移动速度最小值
+			max: 0.5, // 水平移动速度最大值
 		},
 		vertical: {
-			min: 1.5, // 垂直移动速度最小值
-			max: 2.2, // 垂直移动速度最大值
+			min: 1.0, // 垂直移动速度最小值
+			max: 1.3, // 垂直移动速度最大值
 		},
 		rotation: 0.03, // 旋转速度
 		fadeSpeed: 0.03, // 消失速度，不应大于最小不透明度
